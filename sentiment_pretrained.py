aed0abfa1a82bd8fe386fe1827ec5683090822fb
czhroailsky <javier.reyes.ic@gmail.com>
2019-03-24 02:27:19 -0600

    pretrained_test

--- sentiment_pretrained.py
+++ sentiment_pretrained.py
@@ -1,1 +1,10 @@
-# Imports 
+# Imports
+from classifier import *
+import pandas as pd
+
+nacion_unida = pd.read_json('nacion_unida.json')
+
+clf = SentimentClassifier()
+
+for index, row in nacion_unida.head().iterrows():
+    print(row['text'] + ' ==> %.5f' % clf.predict(row['text']))
